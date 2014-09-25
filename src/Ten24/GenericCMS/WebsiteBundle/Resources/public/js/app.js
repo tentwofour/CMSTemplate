23dfcf51302f584ecb9814fa7e349613f1b7d742
blair <blair@tentwentyfour.ca>
2014-09-25 12:53:42 -0600

    Added src dir with default files/buncle structure Added missing config/routing files Updated composer.json to require cms-page-bundle

---
+++ src/Ten24/GenericCMS/WebsiteBundle/Resources/public/js/app.js
@@ -0,0 +1,15 @@
+var genericCMSWebsite = (function($, window, undefined) {
+
+    var init = function() 
+    {
+    };
+    
+    return {
+        init: init
+    };
+
+}(jQuery, window));
+
+$(function() {
+    genericCMSWebsite.init();
+});
---
+++ src/Ten24/GenericCMS/WebsiteBundle/Ten24GenericCMSWebsiteBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ten24\GenericCMS\WebsiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Ten24GenericCMSWebsiteBundle extends Bundle
+{
+
+}
